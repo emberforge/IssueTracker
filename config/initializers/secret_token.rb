ffe023a4b760f67bb46a8fb4232d9a7fa0b338e7
Andrew Prasetya <bkdevstudio@gmail.com>
2012-08-16 11:56:45 -0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IssueTracker::Application.config.secret_token = '82265aa8115bbbb2c04e893e6ba60abc5438e6d7341521c7e2fd73f0d912af2a23c878ffded47ca88f33e907fe5ed1e96f5073fcfd210606ac837126175311d7'
